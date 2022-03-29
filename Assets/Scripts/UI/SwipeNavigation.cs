e49b2ec25b9ecbcfacfe947f6718723a433e28c0
ShiningBreaD <bordakovvbros@gmail.com>
2022-03-29 13:57:09 +0900

    Navigation by swipes

---
+++ Assets/Scripts/UI/SwipeNavigation.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class SwipeNavigation : MonoBehaviour
+{
+    [SerializeField] private Transform navigationObject;
+    [SerializeField] private float speedOfReturn;
+
+    private Vector3 offset;
+    private Vector3 rootedPosition;
+    private bool isCardDragging = false;
+
+    private void Start()
+    {
+        rootedPosition = navigationObject.localPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rootedPosition = 
+            new Vector3(collision.transform.localPosition.x * -1, collision.transform.localPosition.y, collision.transform.localPosition.z);
+    }
+
+    private void Update()
+    {
+        if (!isCardDragging)
+            SetToRootedPosition();
+    }
+
+    private void OnMouseDown()
+    {
+        isCardDragging = true;
+
+        offset = navigationObject.position - 
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, navigationObject.position.y, 10f));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 desiredPosition = new Vector3(Input.mousePosition.x, navigationObject.position.y, 10f);
+        navigationObject.position = Camera.main.ScreenToWorldPoint(desiredPosition) + offset;
+    }
+
+    private void OnMouseUp()
+    {
+        isCardDragging = false;
+    }
+
+    private void SetToRootedPosition()
+    {
+        navigationObject.localPosition = Vector3.Lerp(navigationObject.localPosition, rootedPosition, Time.deltaTime * speedOfReturn);
+    }
+}
